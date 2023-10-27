9e398f3bcd2c9d804cc4e6cc6400b25ffad85dee
ykchoi7 <7dbsrud7@gmail.com>
2023-10-27 16:16:51 +0900

    231027_SpringBootJPA

---
+++ springbootJPA2/src/test/java/com/my/jpa/SpringbootJpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
