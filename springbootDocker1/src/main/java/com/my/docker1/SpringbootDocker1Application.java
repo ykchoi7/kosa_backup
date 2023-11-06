3977cb13e4a69446620ed4d753c30b34d8732e4f
ykchoi7 <7dbsrud7@gmail.com>
2023-11-06 17:25:45 +0900

    231106_Docker1_페이지연결출력

---
+++ springbootDocker1/src/main/java/com/my/docker1/SpringbootDocker1Application.java
@@ -0,0 +1,13 @@
+package com.my.docker1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDocker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDocker1Application.class, args);
+	}
+
+}
---
+++ springbootDocker1/src/test/java/com/my/docker1/SpringbootDocker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.docker1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDocker1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
